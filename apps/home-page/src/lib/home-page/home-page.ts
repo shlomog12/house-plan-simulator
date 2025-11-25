376fe93fef54b9ece5381a46b5563e6c84ef232c
shlomog12 <73135976+shlomog12@users.noreply.github.com>
2025-11-25 21:23:11 +0200

    create home page

--- apps/home-page/src/lib/home-page/home-page.ts
+++ apps/home-page/src/lib/home-page/home-page.ts
@@ -1,10 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-home-page',
   imports: [],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
+  standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {}
+export class HomePage {
+
+
+
+
+  constructor(private router: Router) {}
+
+  createNewProject() {
+    console.log('יצירת פרויקט חדש...');
+    this.router.navigate(['/new-project']);
+  }
+}
